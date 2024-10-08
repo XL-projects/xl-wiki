e9244fde0b7d6fb432d9a85b1bed327679241544
uertyk_ <victor.d.popov@gmail.com>
2024-10-08 17:52:29 +0400

    Deploy website - based on 9f4ff2489c121aaf604a1510defc283ce49d7de0

---
+++ assets/js/36994c47.c6f9b50e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkxl_wiki=self.webpackChunkxl_wiki||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ assets/js/acecf23e.60bbc678.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkxl_wiki=self.webpackChunkxl_wiki||[]).push([[1903],{1912:l=>{l.exports=JSON.parse('{"blogBasePath":"/xl-wiki/blog","blogTitle":"Blog","authorsListPath":"/xl-wiki/blog/authors"}')}}]);
