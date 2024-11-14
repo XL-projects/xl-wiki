b618ce9bc324a86baef9dec1f5c3eb6c2ce54cd3
uertyk_ <victor.d.popov@gmail.com>
2024-11-14 22:19:31 +0400

    uh..

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,6 +25,7 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'rus'],
@@ -70,7 +71,6 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
-
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
