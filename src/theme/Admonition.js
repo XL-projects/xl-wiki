f7276abcc2990a19919c98393a74e93fc885be82
uertyk_ <151397011+uertyk@users.noreply.github.com>
2024-11-14 22:58:26 +0400

    Create Admonition.js

---
+++ src/theme/Admonition.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Admonition from '@theme-original/Admonition';
+import MyCustomNoteIcon from '@site/static/img/info.svg';
+
+export default function AdmonitionWrapper(props) {
+  if (props.type !== 'info') {
+    return <Admonition title="My Custom Admonition Title" {...props} />;
+  }
+  return <Admonition icon={<MyCustomNoteIcon />} {...props} />;
+}
