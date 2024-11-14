b0be2bc5bfa4ff5a7284e0be47922ee31b76e907
uertyk_ <151397011+uertyk@users.noreply.github.com>
2024-11-15 00:19:19 +0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/XL-projects/xl-wiki/edit/main/',
         },
         blog: {
           showReadingTime: true,
