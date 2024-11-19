104d4a7e4fb6c5f22848236e922dfba7050a7076
uertyk_ <151397011+uertyk@users.noreply.github.com>
2024-11-19 12:54:27 +0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -107,7 +107,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro.mdx',
+              to: '/docs/intro',
             },
           ],
         },
